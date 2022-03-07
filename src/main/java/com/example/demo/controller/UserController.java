1376c5ae3a7cc8ebed3492992d8cb8d8abe22df3
VishalR999 <vishal.r@gadgeon.com>
2022-03-07 10:02:12 +0530

    user name

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,10 @@ public class UserController {
         return new ResponseEntity<>("This response is from User API", HttpStatus.OK);
     }
 
+    @GetMapping("/user/{name}")
+    public ResponseEntity<String> getUserName(@PathVariable(value = "name") String name) {
+        return new ResponseEntity<>("This response is from User : " + name, HttpStatus.OK);
+    }
 
     @GetMapping("/")
     public ResponseEntity<String> emptyAPI() {
